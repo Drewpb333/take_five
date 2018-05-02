ab496c9d56176ff2247dc81b16c0eb60360fb715
Will Miller <willmiller@Wills-MacBook-Pro.local>
2018-05-02 09:20:04 -0400

    heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "take_five"
-});
+
+
+if (process.env.JAWSDB_URL) {
+    connection.mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "take_five"
+    });
+}
 
 connection.connect(function(err) {
     if (err) {
